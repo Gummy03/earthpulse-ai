c00001b5e7bef11fdedeb2a168f810134eb8b020
Gummy03 <chitumallik96@gmail.com>
2026-02-26 22:25:54 +0530

    full radar anomaly dashboard

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
-Cesium.Ion.defaultAccessToken = 'PASTE_TOKEN';
+Cesium.Ion.defaultAccessToken = 'PASTE_TOKEN_HERE';
 
 var viewer = new Cesium.Viewer('cesiumContainer', {
-animation: true,
-timeline: true,
-baseLayerPicker: true,
-terrainProvider: new Cesium.EllipsoidTerrainProvider()
+animation:true,
+timeline:true,
+baseLayerPicker:true,
+terrainProvider:new Cesium.EllipsoidTerrainProvider()
 });
 
 fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")
-.then(res => res.json())
-.then(data => {
+.then(res=>res.json())
+.then(data=>{
 
-data.features.forEach(eq => {
+data.features.forEach(eq=>{
 
 const coords = eq.geometry.coordinates;
 const mag = eq.properties.mag || 0;
 
+// magnitude color
 let color = Cesium.Color.YELLOW;
-if(mag > 4) color = Cesium.Color.ORANGE;
-if(mag > 6) color = Cesium.Color.RED;
+if(mag>4) color = Cesium.Color.ORANGE;
+if(mag>6) color = Cesium.Color.RED;
 
-// main anomaly point
+// main marker
 viewer.entities.add({
-position: Cesium.Cartesian3.fromDegrees(coords[0], coords[1]),
+position:Cesium.Cartesian3.fromDegrees(coords[0],coords[1]),
 point:{
 pixelSize:8,
 color:color,
@@ -32,9 +33,9 @@ outlineWidth:1
 description:`<b>${eq.properties.place}</b><br>Magnitude: ${mag}`
 });
 
-// radar ring
+// anomaly ring
 viewer.entities.add({
-position: Cesium.Cartesian3.fromDegrees(coords[0], coords[1]),
+position:Cesium.Cartesian3.fromDegrees(coords[0],coords[1]),
 ellipse:{
 semiMinorAxis:20000,
 semiMajorAxis:20000,
@@ -43,22 +44,9 @@ height:0
 }
 });
 
-});
-
-// camera focus on biggest quake
-const biggest = data.features.reduce((a,b)=>
-(a.properties.mag||0)>(b.properties.mag||0)?a:b);
-
-const bc = biggest.geometry.coordinates;
-viewer.camera.flyTo({
-destination: Cesium.Cartesian3.fromDegrees(bc[0], bc[1], 2000000)
-});
-
-});
-
-// radar scan effect
+// radar stripe scan
 viewer.entities.add({
-position: Cesium.Cartesian3.fromDegrees(coords[0], coords[1]),
+position:Cesium.Cartesian3.fromDegrees(coords[0],coords[1]),
 ellipse:{
 semiMinorAxis:50000,
 semiMajorAxis:50000,
@@ -70,16 +58,29 @@ repeat:1
 }
 });
 
-
+// pulsing radar animation
 viewer.entities.add({
-position: Cesium.Cartesian3.fromDegrees(coords[0], coords[1]),
+position:Cesium.Cartesian3.fromDegrees(coords[0],coords[1]),
 ellipse:{
-semiMinorAxis: new Cesium.CallbackProperty(function(time){
+semiMinorAxis:new Cesium.CallbackProperty(function(){
 return 20000 + (Date.now()%2000)*20;
-}, false),
-semiMajorAxis: new Cesium.CallbackProperty(function(time){
+},false),
+semiMajorAxis:new Cesium.CallbackProperty(function(){
 return 20000 + (Date.now()%2000)*20;
-}, false),
-material: color.withAlpha(0.2)
+},false),
+material:color.withAlpha(0.2)
 }
+});
+
+});
+
+// auto focus biggest quake
+const biggest=data.features.reduce((a,b)=>
+(a.properties.mag||0)>(b.properties.mag||0)?a:b);
+
+const bc=biggest.geometry.coordinates;
+viewer.camera.flyTo({
+destination:Cesium.Cartesian3.fromDegrees(bc[0],bc[1],2000000)
+});
+
 });
